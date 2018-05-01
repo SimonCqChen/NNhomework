460d10fc444e68d1dfc74e1d5b50b546e2c3012d
SimonCqChen <864276980@qq.com>
2018-05-01 21:11:42 +0800

    作业四

--- Homework4/lstm.py
+++ Homework4/lstm.py
@@ -10,12 +10,12 @@ INPUT_SIZE = 310
 SHORTEST_LENGTH = 185
 TIME_STEP = 5
 HIDDEN_NODE_NUM = 256
-LAYER_NUM = 2
 OUTPUT_SIZE = 3
 BATCH_SIZE = 9 * int(SHORTEST_LENGTH / TIME_STEP)
 
 x_ = tf.placeholder(tf.float32, [None, TIME_STEP, INPUT_SIZE])
 y_ = tf.placeholder(tf.float32, [None, OUTPUT_SIZE])
+batch_size = tf.placeholder(tf.int32, [])
 
 weights = {
     'in': tf.Variable(tf.random_normal([INPUT_SIZE, HIDDEN_NODE_NUM])),
@@ -27,19 +27,20 @@ biases = {
 }
 
 
-def rnn(X, weights, biases):
+def rnn(X, weights, biases, batch_size):
     X = tf.reshape(X, [-1, INPUT_SIZE])
     X_in = tf.matmul(X, weights['in']) + biases['in']
     X_in = tf.reshape(X_in, [-1, TIME_STEP, HIDDEN_NODE_NUM])
     lstm = tf.nn.rnn_cell.BasicLSTMCell(HIDDEN_NODE_NUM, forget_bias=1.0, state_is_tuple=True)
-    init_state = lstm.zero_state(BATCH_SIZE, dtype=tf.float32)
+    init_state = lstm.zero_state(batch_size, dtype=tf.float32)
     outputs, final_state = tf.nn.dynamic_rnn(lstm, X_in, initial_state=init_state, time_major=False)
 
-    results = tf.matmul(final_state[1], weights['out']) + biases['out']
+    outputs = tf.unstack(tf.transpose(outputs, [1,0,2]))
+    results = tf.matmul(outputs[-1], weights['out']) + biases['out']
     return results
 
 
-pred = rnn(x_, weights, biases)
+pred = rnn(x_, weights, biases, batch_size)
 # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y_))
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y_))
 train_op = tf.train.AdamOptimizer(LEARNING_RATE).minimize(cost)
@@ -94,27 +95,167 @@ def get_train_test_label():
 train_data_1, test_data_1 = get_train_test_data('1')
 train_data_2, test_data_2 = get_train_test_data('2')
 train_data_3, test_data_3 = get_train_test_data('3')
-train_label, test_label = get_train_test_label()
+train_label_temp, test_label_temp = get_train_test_label()
+train_data_1 = np.reshape(train_data_1, (-1, TIME_STEP, INPUT_SIZE))
+train_data_2 = np.reshape(train_data_2, (-1, TIME_STEP, INPUT_SIZE))
+train_data_3 = np.reshape(train_data_3, (-1, TIME_STEP, INPUT_SIZE))
+test_data_1 = np.reshape(test_data_1, (-1, TIME_STEP, INPUT_SIZE))
+test_data_2 = np.reshape(test_data_2, (-1, TIME_STEP, INPUT_SIZE))
+test_data_3 = np.reshape(test_data_3, (-1, TIME_STEP, INPUT_SIZE))
+train_label = []
+test_label = []
+for label in train_label_temp:
+    for i in range(0, int(SHORTEST_LENGTH / TIME_STEP)):
+        train_label.append(label)
+for label in test_label_temp:
+    for j in range(0, int(SHORTEST_LENGTH / TIME_STEP)):
+        test_label.append(label)
 
 init = tf.global_variables_initializer()
-with tf.Session() as sess:
-    sess.run(init)
-    for step in range(0, TRAIN_STEP):
-        x, labels = train_data_1, train_label
-        x = np.reshape(x, (-1, TIME_STEP, INPUT_SIZE))
-        y = []
-        for label in labels:
-            for i in range(0, int(SHORTEST_LENGTH / TIME_STEP)):
-                y.append(label)
-        sess.run([train_op], feed_dict={
-            x_: x,
-            y_: y,
-        })
-        if step % 50 == 0:
-            print(
-                sess.run(cost, feed_dict={
-                    x_: x,
-                    y_: y})
-                # sess.run(weights['out'])
-                )
-            print('======================================')
+
+
+def run_lstm():
+    with tf.Session() as sess:
+        accuracy_file = open('accuracy_file_01.csv', 'w')
+        accuracy_file.write('step,accuracy\n')
+        cost_file = open('cost_file_01.csv', 'w')
+        cost_file.write('step,cost\n')
+        sess.run(init)
+        for step in range(0, TRAIN_STEP):
+            x, y = train_data_1, train_label
+            sess.run([train_op], feed_dict={
+                x_: x,
+                y_: y,
+                batch_size: 333
+            })
+            if step % 50 == 0:
+                print(
+                    sess.run(correct_prediction, feed_dict={
+                        x_: test_data_1,
+                        y_: test_label,
+                        batch_size: 222})
+                    # sess.run(weights['out'])
+                    )
+                print('======================================')
+                cost_value = sess.run(cost, feed_dict={
+                        x_: x,
+                        y_: y,
+                        batch_size: 333})
+                accu_value = sess.run(accuracy, feed_dict={
+                    x_: test_data_1,
+                    y_: test_label,
+                    batch_size: 222})
+                accuracy_file.write(str(step) + ',' + str(accu_value) + '\n')
+                cost_file.write(str(step) + ',' + str(cost_value) + '\n')
+        accuracy_file.close()
+        cost_file.close()
+
+    with tf.Session() as sess:
+        accuracy_file = open('accuracy_file_02.csv', 'w')
+        accuracy_file.write('step,accuracy\n')
+        cost_file = open('cost_file_02.csv', 'w')
+        cost_file.write('step,cost\n')
+        sess.run(init)
+        for step in range(0, TRAIN_STEP):
+            x, y = train_data_2, train_label
+            sess.run([train_op], feed_dict={
+                x_: x,
+                y_: y,
+                batch_size: 333
+            })
+            if step % 50 == 0:
+                # print(
+                #     sess.run(cost, feed_dict={
+                #         x_: test_data_1,
+                #         y_: test_label,
+                #         batch_size: 222})
+                #     # sess.run(weights['out'])
+                #     )
+                # print('======================================')
+                cost_value = sess.run(cost, feed_dict={
+                        x_: x,
+                        y_: y,
+                        batch_size: 333})
+                accu_value = sess.run(accuracy, feed_dict={
+                    x_: test_data_2,
+                    y_: test_label,
+                    batch_size: 222})
+                accuracy_file.write(str(step) + ',' + str(accu_value) + '\n')
+                cost_file.write(str(step) + ',' + str(cost_value) + '\n')
+        accuracy_file.close()
+        cost_file.close()
+
+    with tf.Session() as sess:
+        accuracy_file = open('accuracy_file_03.csv', 'w')
+        accuracy_file.write('step,accuracy\n')
+        cost_file = open('cost_file_03.csv', 'w')
+        cost_file.write('step,cost\n')
+        sess.run(init)
+        for step in range(0, TRAIN_STEP):
+            x, y = train_data_3, train_label
+            sess.run([train_op], feed_dict={
+                x_: x,
+                y_: y,
+                batch_size: 333
+            })
+            if step % 50 == 0:
+                # print(
+                #     sess.run(cost, feed_dict={
+                #         x_: test_data_1,
+                #         y_: test_label,
+                #         batch_size: 222})
+                #     # sess.run(weights['out'])
+                #     )
+                # print('======================================')
+                cost_value = sess.run(cost, feed_dict={
+                        x_: x,
+                        y_: y,
+                        batch_size: 333})
+                accu_value = sess.run(accuracy, feed_dict={
+                    x_: test_data_3,
+                    y_: test_label,
+                    batch_size: 222})
+                accuracy_file.write(str(step) + ',' + str(accu_value) + '\n')
+                cost_file.write(str(step) + ',' + str(cost_value) + '\n')
+        accuracy_file.close()
+        cost_file.close()
+
+
+def run_lstm_concat():
+    with tf.Session() as sess:
+        accuracy_file = open('accuracy_file_concat.csv', 'w')
+        accuracy_file.write('step,accuracy\n')
+        cost_file = open('cost_file_concat.csv', 'w')
+        cost_file.write('step,cost\n')
+        sess.run(init)
+
+        x, y = list(train_data_1) + list(train_data_2) + list(train_data_3), train_label + train_label + train_label
+        test_data_concat, test_label_concat = \
+            list(test_data_1) + list(test_data_2) + list(test_data_3), test_label + test_label + test_label
+        for step in range(0, TRAIN_STEP):
+            sess.run([train_op], feed_dict={
+                x_: x,
+                y_: y,
+                batch_size: 999
+            })
+            if step % 50 == 0:
+                # print(
+                #     sess.run(cost, feed_dict={
+                #         x_: test_data_1,
+                #         y_: test_label,
+                #         batch_size: 222})
+                #     # sess.run(weights['out'])
+                #     )
+                # print('======================================')
+                cost_value = sess.run(cost, feed_dict={
+                        x_: x,
+                        y_: y,
+                        batch_size: 999})
+                accu_value = sess.run(accuracy, feed_dict={
+                    x_: test_data_concat,
+                    y_: test_label_concat,
+                    batch_size: 666})
+                accuracy_file.write(str(step) + ',' + str(accu_value) + '\n')
+                cost_file.write(str(step) + ',' + str(cost_value) + '\n')
+        accuracy_file.close()
+        cost_file.close()
